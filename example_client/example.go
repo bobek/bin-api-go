f7a57ac74f81bb638fd14ef08b78cf788c0b920c
Antonin Kral <a.kral@bobek.cz>
2021-01-25 14:18:52 +0100

    Update crude client example

--- example_client/example.go
+++ example_client/example.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"os"
 
 	"github.com/bobek/bin-api-go/v1/binapi"
 )
@@ -12,10 +13,24 @@ import (
 func main() {
 	println("Testing...")
 
-	client := binapi.NewClient(nil, "secret_auth_token")
+	client := binapi.NewClient(nil, "client-test", os.Getenv("HMAC_TOKEN"))
 	fmt.Println(client)
 
-	user, resp, _ := client.Users.Get(context.Background(), "bobek")
-	fmt.Println(user)
+	//user, resp, _ := client.Users.Get(context.Background(), "bobek")
+	//fmt.Println(user)
+	//fmt.Println(resp)
+
+	ups, resp, _ := client.UserProperties.List(context.Background())
+	fmt.Println(ups)
 	fmt.Println(resp)
+
+	up, resp, err := client.UserProperties.Get(context.Background(), "str_property")
+	fmt.Println(up)
+	fmt.Printf("%+v\n", resp)
+	fmt.Printf("%+v\n", err)
+
+	upn, resp, err := client.UserProperties.Get(context.Background(), "non_existing")
+	fmt.Println(upn)
+	fmt.Printf("%+v\n", resp)
+	fmt.Printf("%+v\n", err)
 }
